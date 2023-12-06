c25369c337a103af781d3098fe5a9539d2fae72e
Máy của Tùng <tung_28@192.168.1.8>
2023-12-06 17:52:46 +0700

    update

--- screens/CameraScreen.jsx
+++ screens/CameraScreen.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import {Camera, useCameraDevice} from 'react-native-vision-camera';
-import {StyleSheet, View, TouchableOpacity, Dimensions, Image} from 'react-native'; 
+import {StyleSheet, View, TouchableOpacity, Dimensions, Image, Text} from 'react-native'; 
 import ShutterBtn from '../assets/shutter.svg';
 import FlipCameraBtn from '../assets/flipCamera.svg';
 import CloseBtn from '../assets/close.svg';
+import { height } from 'deprecated-react-native-prop-types/DeprecatedImagePropType';
+import RNFS from 'react-native-fs';
 
 const CameraScreen = () => {
     const [camView, setCamView] = useState('back'); 
@@ -12,7 +14,7 @@ const CameraScreen = () => {
     const device = useCameraDevice(camView, {
         physicalDevices: ['wide-angle-camera']
      });
-     const [imageSource, setImageSource] = useState('');
+     const [imageSource, setImageSource] = useState(null);
      const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
      useEffect(() => {
@@ -22,13 +24,20 @@ const CameraScreen = () => {
          }
          getPermisstion();
      }, [])
- 
+    const flipCamera = () => {
+        if (camView === 'back') {
+            setCamView('front');
+        } else {
+            setCamView('back');
+        }
+    }   
      const capturePhoto = async () => {
          if (camera.current !== null){
             try {
                 const photo = await camera.current.takePhoto()
                 setShowCamera(false);
-                setImageSource(photo.path);
+                setImageSource(photo);
+                
             } catch(error) {
                 return error;
             }
@@ -41,6 +50,44 @@ const CameraScreen = () => {
     
     const recapture = () => {
         setShowCamera(true);
+    }
+
+    const getImageFile = async (imagePath) => {
+        try {
+          const imageFile = await RNFS.readFile(imagePath, 'base64');
+          return imageFile;
+        } catch (error) {
+          console.error('Error reading image file:', error);
+        }
+      };
+
+    const startTransfrom = async () => {
+        const formData = new FormData();
+        console.log(imageSource)
+        formData.append('file_upload', getImageFile(imageSource.path));
+
+        try {
+            const endPoint = 'https://c827-114-34-116-46.ngrok-free.app/uploadfile';
+            const response = await fetch(endPoint, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },            
+            }).then(response => response.json())
+            .then(response => {
+              console.log('response', response)
+            })
+
+            if (response.ok) {
+                console.log("File uploaded successfully")
+            } else {
+                console.log(response)
+            }
+
+        } catch (error) {
+            return error
+        }
     }
 
     const styles = StyleSheet.create({
@@ -64,11 +111,26 @@ const CameraScreen = () => {
             height: 120,
         },
         closeBtn: {
+            width: 30,
+            height: 30,
+        },
+        afterCapture: {
             position: 'absolute',
             top: viewportHeight - 130,
             left: 20,
-            width: 30,
-            height: 30,
+            flexDirection: 'row',
+            gap: 30
+        },
+        transformBtn: {
+            width: 100,
+            height: 45,
+            backgroundColor: '#3d5c98',
+            borderRadius: 5,
+            alignItems: 'center'
+        },
+        transformText: {
+            color: '#fff',
+            height: 45
         }
     })
 
@@ -88,15 +150,20 @@ const CameraScreen = () => {
                 <Image
                     style={styles.image}
                     source={{
-                        uri: `file://${imageSource}`,
+                        uri: `file://${imageSource !== null ? imageSource.path : ' '}`,
                     }}
                 />
-                <TouchableOpacity
-                    style={styles.closeBtn}
-                    onPress={recapture}
-                >
-                    <CloseBtn />
-                </TouchableOpacity>
+                <View style={styles.afterCapture}>
+                    <TouchableOpacity
+                        style={styles.closeBtn}
+                        onPress={recapture}
+                    >
+                        <CloseBtn />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.transformBtn} onPress={startTransfrom}>
+                        <Text style={styles.transformText}>Transform</Text>
+                    </TouchableOpacity>
+                </View>
             </>
             )
         )}
@@ -117,6 +184,7 @@ const CameraScreen = () => {
             </TouchableOpacity>
             <TouchableOpacity
                 style={styles.cameraFlipBtn}
+                onPress={flipCamera}
             >
                 <FlipCameraBtn style={styles.flipCameraBtn} />
             </TouchableOpacity>
